e918d5027c0a610af73194a18edd32b4b63995f7
Ervinas Mitkevicius <ervinas.mitkevicius@gmail.com>
2023-01-25 19:42:59 +0200

    helpers: constructors, methods

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -1,9 +1,32 @@
-type CarsCollectionProps = {
+import Car from '../types/car';
+import Brand from '../types/brand';
+import Model from '../types/model';
+import CarJoined from '../types/car-joined';
 
+type CarsCollectionProps = {
+    cars: Car[],
+    brands: Brand[],
+    models: Model[],
 };
 
 class CarsCollection {
+    private props: CarsCollectionProps;
 
+    constructor(props: CarsCollectionProps) {this.props = props;}
+
+    private joinCar = ({ model_id, ...car}: Car) => 
+        {
+            const {brands, models} = this.props;
+            const carBrand = brands.find((brand) => brand.id === carModel?.brand_id);
+            const carModel = models.find((model) => model.id === model_id);
+
+            return {...car,brand: (carBrand && carBrand.title) ?? 'undefined', model: (carModel && carModel.title) ?? 'undefined',};
+        };
+
+    public get all(): CarJoined[] 
+        {
+            return this.props.cars.map(this.joinCar);
+        }
 }
 
 export default CarsCollection;
