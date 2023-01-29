69d759ec3515beccaa7e24979a45ca37ae387cd5
Ervinas Mitkevicius <ervinas.mitkevicius@gmail.com>
2023-01-29 22:10:19 +0200

    table: added row/column checking

--- src/components/table.ts
+++ src/components/table.ts
@@ -1,3 +1,5 @@
+import countObjectProperties from '../helpers/count-object-properties';
+
 export type TableProps<Type> = {
   title: string,
   columns: Type,
@@ -20,12 +22,32 @@ class Table<Type extends RowData> {
 
   public constructor(props: TableProps<Type>) {
     this.props = props;
+    this.columnCheck();
     this.htmlElement = document.createElement('table');
     this.thead = document.createElement('thead');
     this.tbody = document.createElement('tbody');
 
     this.initialize();
   }
+
+  private columnCheck = (): void => {
+    const { rowsData, columns } = this.props;
+
+    if (this.props.rowsData.length === 0) return;
+    const columnCount = countObjectProperties(columns);
+
+    const columnsEqualRows = rowsData.every((row) => {
+      const rowCellsCount = countObjectProperties(row);
+
+      return rowCellsCount === columnCount;
+    });
+
+    if (!columnsEqualRows) {
+      throw new Error(
+        'The column and row quantity is different',
+      );
+    }
+  };
 
 private initializeHead = () => {
     const { title, columns } = this.props;
---
+++ src/helpers/count-object-properties.ts
@@ -0,0 +1,2 @@
+const countObjectProperties = (object: Object): number => Object.keys(object).length;
+export default countObjectProperties;
